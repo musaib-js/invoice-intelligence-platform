be13dd1914b0e59259e8b80a956218ce54ed3e86
Musaib Altaf <85894666+musaib-js@users.noreply.github.com>
2023-11-19 21:10:02 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,19 +4,6 @@ import PDFTableComponent from "./components/PDFTableComponent";
 function App() {
   return (
     <div className="App">
-      {/* <nav
-        className="navbar p-3 shadow-sm"
-        style={{ backgroundColor: "#FDFFD0", marginBottom: "55px" }}
-      >
-        <div className="container-fluid">
-          <span
-            className="navbar-brand mb-0 h1 m-auto"
-            style={{ fontSize: "1.4em", letterSpacing: "1px" }}
-          >
-            Invoice Intelligence Platform
-          </span>
-        </div>
-      </nav> */}
       <PDFTableComponent />
 
     </div>
